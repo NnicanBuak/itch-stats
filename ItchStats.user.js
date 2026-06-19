a4450cce1c34fcecb46d22847ab65839eb0cef87
Nnican <89499782+NnicanBuak@users.noreply.github.com>
2026-06-20 00:45:59 +0800

    Bump itch.io stats to 6.4.0

--- ItchStats.user.js
+++ ItchStats.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         itch.io stats
 // @namespace    https://itch.io/
-// @version      6.3.9
+// @version      6.4.0
 // @description  Ищет свои игры в списках itch.io, сохраняет позиции, показывает статистику и пассивно подсвечивает найденные игры
 // @match        https://itch.io/*
 // @match        https://*.itch.io/*
